3dd4abfeed21e3545bdaa01a1c46e63d332baa87
prussian-1to9 <prussian.3to3@gmail.com>
2024-06-24 23:02:04 +0900

    [TEST] next js project testing

---
+++ next.config.js
@@ -0,0 +1,6 @@
+const prefix = process.env.NODE_ENV === 'production' ? 'https://prussian-1to9.github.io/prussian-1to9.github.io' : '';
+
+const nextConfig = {
+    output: 'export',
+    assetPrefix: '',
+}
